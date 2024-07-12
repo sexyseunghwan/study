fa5ec4321f4775783af4d09bcd1d123259806407
seunghwan <ssh9308@naver.com>
2024-07-12 18:17:03 +0900

    2024-07-12
    2024-07-12

--- 업무중막힌것/Python/matplot_error.sh
+++ 업무중막힌것/Python/matplot_error.sh
@@ -24,3 +24,23 @@
 
 ## 설정파일 위치로 들어가서 matplotlibrc 파일을 열어서 font.family 쪽 부분 주석을 해제하고 원하는 서체의 이름을 입력하면 된다.
 ## 그다음 캐시 위치로 이동해서 fontlist-~.json 파일을 지워주고 다시 프로그램 실행하니 해결되었다.
+
+
+
+(base)  we@2022080007  ~/Documents/work_code/python_graph_api   main  python3
+Python 3.12.4 (main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import matplotlib as mpl
+>>> import matplotlib.pyplot as plt
+>>> import matplotlib.font_manager as fm
+>>> print ('버전: ', mpl.__version__)
+버전:  3.7.5
+>>> print ('설치 위치: ', mpl.__file__)
+설치 위치:  /usr/local/lib/python3.12/site-packages/matplotlib/__init__.py
+>>> print ('설정 위치: ', mpl.get_configdir())
+설정 위치:  /Users/we/.matplotlib
+>>> print ('캐시 위치: ', mpl.get_cachedir())
+캐시 위치:  /Users/we/.matplotlib
+>>> print ('설정파일 위치: ', mpl.matplotlib_fname())
+설정파일 위치:  /usr/local/lib/python3.12/site-packages/matplotlib/mpl-data/matplotlibrc
+>>> 

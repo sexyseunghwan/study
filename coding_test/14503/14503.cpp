41ad51e6d73b8a41093132d5bf424ebcea15f8d7
sexyseunghwan <ssh9308@naver.com>
2024-07-30 21:09:10 +0900

    2024-07-30
    2024-07-30

---
+++ coding_test/14503/14503.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int N,M;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+
+    cin >> N >> M;
+
+    
+    
+
+
+    return 0;
+}

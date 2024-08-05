aab2c9dd20114f822d96106194b951aed41a01dd
sexyseunghwan <ssh9308@naver.com>
2024-08-06 00:11:47 +0900

    2024-08-06
    2024-08-06

--- coding_test/2665/2665.cpp
+++ coding_test/2665/2665.cpp
@@ -7,32 +7,41 @@ using namespace std;
 int N;
 int map[50][50];
 int count_map[50][50];
-//int count_map[50][50];
+int visited_map[50][50];
 int dr[4] = {0,0,-1,1};
 int dc[4] = {1,-1,0,0};
 
 
 void bfs(int r, int c)
 {
-    //count_map[r][c] = 0;
     queue<pair<int, int>> que;
     que.push(make_pair(r,c));
+    visited_map[r][c] = 1;
 
     while(!que.empty())
     {
         int row = que.front().first;
         int col = que.front().second;
         que.pop();
-        int chg_cnt = map[row][col];
 
         for (int i = 0; i < 4; i++)
         {
             int new_r = row + dr[i];
             int new_c = col + dc[i];
             
-            if (new_r >= 0 && new_r < N && new_c >= 0 && new_c < N && count_map[new_r][new_c] == 0)
+            if (new_r >= 0 && new_r < N && new_c >= 0 && new_c < N)
             {
-                            
+                int count_cnt = count_map[r][c];
+                if (map[new_r][new_c] == 0) count_cnt++;
+                
+                if (visited_map[new_r][new_c] == 0) {
+                    count_map[new_r][new_c] = count_cnt;
+                    visited_map[new_r][new_c] = 1;
+                    que.push(make_pair(new_r, new_c));
+                } else if (count_map[new_r][new_c] > count_cnt) {
+                    count_map[new_r][new_c] = count_cnt;
+                    que.push(make_pair(new_r, new_c));
+                }   
             }
         }
     }
@@ -50,5 +59,7 @@ int main()
     
     bfs(0,0);    
     
+    cout << count_map[N-1][N-1] << endl;
+
     return 0;
 }
--- coding_test/compile.sh
+++ coding_test/compile.sh
@@ -13,9 +13,5 @@ g++ -std=c++17 -Wall -Wextra -o 4963_bfs 4963_bfs.cpp
 
 g++ -std=c++17 -Wall -Wextra -o 14503 14503.cpp
 
-4 4
-2 2 0
-1 1 1 1
-1 0 0 1
-1 0 0 1
-1 1 1 1
+g++ -std=c++17 -Wall -Wextra -o 2665 2665.cpp
+
